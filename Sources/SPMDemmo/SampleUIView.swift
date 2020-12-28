8e1bfe3bacd9f83b67b1215115e3e2507b3752a9
Nirmal Kumar S <nsubramnaiyan@follett.com>
2020-12-28 15:47:26 +0530

    Initial Commit2

---
+++ Sources/SPMDemmo/SampleUIView.swift
@@ -0,0 +1,34 @@
+//
+//  SampleUIView.swift
+//  SPMDemo
+//
+//  Created by Nirmal Kumar S on 23/12/20.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+import UIKit
+
+@available(iOS 13.0.0, *)
+public struct SampleUIView: SwiftUI.View {
+	public var body: some View {
+		VStack {
+			Text("Hello, world!")
+			Text("Hello, world!")
+			Text("Hello, world!")
+			Text("Hello, world!")
+			Text("Hello, world!")
+		}.frame(width: 200, height: 200, alignment: .center)
+		.background(Color(.lightGray))
+		.padding()
+		
+	}
+}
+
+//@available(iOS 13.0.0, *)
+//public struct SampleUIView_Previews: PreviewProvider {
+//	public static var previews: some View {
+//		SampleUIView()
+//	}
+//}
