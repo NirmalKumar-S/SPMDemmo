7d7837ba0a9fbd51d17c5c1507cbc85c08af8d8d
Nirmal Kumar S <nsubramnaiyan@follett.com>
2020-12-28 15:16:38 +0530

    Initial Commit2

--- Sources/SPMDemmo/SPMDemmo.swift
+++ Sources/SPMDemmo/SPMDemmo.swift
@@ -1,15 +1,21 @@
-struct SPMDemmo {
-    var text = "Hello, World!"
+
+struct SPMDemmoDefault {
+    var text = "SPM DEMO Loaded default"
 }
 
-public struct SPMDemo2New {
+public struct SPMDemmo1New {
+    var text = "SPM DEMO Loaded 1"
+}
+
+open struct SPMDemo2New {
     public var text = "SPM DEMO Loaded 2"
 }
 
-public func testDemo() -> String {
-	return "SPM Module loaded"
+open struct SPMDemo3New {
+    open var text = "SPM DEMO Loaded 3"
 }
 
-func testDemo2() -> String {
-	return "SPM Module loaded 2"
+public func SPMDemo4New() -> String {
+	return "SPM Module loaded 4"
 }
+
